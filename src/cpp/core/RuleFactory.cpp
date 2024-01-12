a6003d09ce05e9269bb307808fa13327f37b0ac6
nzteb <patrickmbetz@gmail.com>
2024-01-12 16:13:50 +0100

    Allow entities with parentheses in name

--- src/cpp/core/RuleFactory.cpp
+++ src/cpp/core/RuleFactory.cpp
@@ -513,9 +513,12 @@ void RuleFactory::parseAtom(const std::string& input, strAtom& atom) {
     if (!std::getline(stream, atom[1], ',')) {
         throw std::runtime_error("Error when parsing string in parseAtom unexpected format:" + input);
     }
-    //assign tail
-    if (!std::getline(stream, atom[2], ')')) {
-        throw std::runtime_error("Error when parsing string in parseAtom unexpected format:" + input);
+    //assign tail, parse until LAST occurence of ")"; allows entities in yago such as "ent (player)"
+    int last_paren_pos = input.find_last_of(')');
+    if (last_paren_pos == std::string::npos) {
+        throw std::runtime_error("Error when parsing string in parseAtom: no closing parenthesis found in input:" + input);
+    } else {
+        atom[2] = input.substr(0, last_paren_pos);
     }
 }
 
